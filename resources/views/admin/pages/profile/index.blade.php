a980c67f4d810d9fb345c33b9530c26e1d7e4d56
reza <rdaqrbanzadh206@gmail.com>
2023-12-26 17:34:02 +0330

    update profileController

--- resources/views/admin/pages/profile/index.blade.php
+++ resources/views/admin/pages/profile/index.blade.php
@@ -8,6 +8,19 @@
 
     </div>
 
+    @if(Session::has('message'))
+
+        <div role="alert">
+            <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                مشکل
+            </div>
+            <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                {{Session("message")}}
+
+            </div>
+        </div>
+    @endif
+
     {{-- ProfilePage --}}
     <div class="flex space-x-6">
         <div class="flex-2 m-6 self-center">
