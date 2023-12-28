6f1157001c42e0afe303cb4bc0fc1e3d9f492cf3
reza <rdaqrbanzadh206@gmail.com>
2023-12-28 13:06:36 +0330

    send sms code

---
+++ app/Models/SmsCode.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class SmsCode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'mobile',
+        'code'
+    ];
+
+    public static function checkTwoMinute($mobile)
+    {
+        $check = self::query()->where('mobile', $mobile)
+            ->where('created_at', '>', Carbon::now()->subMinute(2))->first();
+        if($check){
+            return true;
+        }
+        return false;
+
+    }
+
+    public static function createSmsCode($mobile, $code)
+    {
+        self::query()->create([
+            'mobile'=>$mobile,
+            'code'=>$code
+        ]);
+    }
+
+    public static function checkSend($mobile, $code)
+    {
+        $check = self::query()->where([
+            'mobile' => $mobile,
+            'code' => $code
+        ])->first();
+
+        if($check){
+            return true;
+        }
+        return false;
+    }
+
+}
