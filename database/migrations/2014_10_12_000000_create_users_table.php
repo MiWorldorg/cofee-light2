11de91ac29ab5c796eb5dbd488bc78247c5f1d06
rezaghorbanzadh <rdaqrbanzadh206@gmail.com>
2023-12-30 13:36:24 +0330

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
+            $table->string('name')->nullable();
+            $table->string('username')->nullable();
             $table->string('password');
             $table->string('phone_number')->unique();
             $table->string('type')->default("user");
